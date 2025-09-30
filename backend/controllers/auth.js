f533837b38c06baef47344ed8037f862ae25bdce
val <valeriabaleanu@gmail.com>
2025-09-30 16:21:31 +0200

    Creazione funzione logout
    Implementazione della funzione logout che elimina il token di autenticazione

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -57,5 +57,11 @@ export async function login (req, res) {
 }
 
 export async function logout (req, res) {
-    res.send("Logout route");
+    try {
+        res.clearCookie("jwt-flashcards");
+        res.status(200).json({success: true, message: "Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout: ", error.message);
+        return res.status(500).json({success: false, message: "Internal server error"});
+    }
 }
