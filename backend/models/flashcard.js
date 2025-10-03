1f504cfa01856755fa1c6a7a7aa9c452120a8420
val <valeriabaleanu@gmail.com>
2025-10-03 13:12:04 +0200

    Update model/flashcard.js
    Modifica modello in modo tale da rendere ogni flashcard legata ad una collezione, invece di un utente

--- backend/models/flashcard.js
+++ backend/models/flashcard.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const flashcardSchema = new mongoose.Schema({
-    user: {
+    collection: {
 			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
+			ref: "Collection",
 			required: true,
 		},
     title:{
