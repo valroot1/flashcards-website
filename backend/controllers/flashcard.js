3504382c09d9a31a0f12a49cf62bee2b415739c8
val <valeriabaleanu@gmail.com>
2025-10-03 16:28:50 +0200

    Creazione metodo addFlashcard

--- backend/controllers/flashcard.js
+++ backend/controllers/flashcard.js
@@ -4,10 +4,10 @@ export async function getFlashcardbyId(req, res) {
     const { id } = req.params;
     try {
         const data = await Flashcard.findById(id);
-        return res.status(200).json({success:true, message:"Flashcard obtained", data: data});
+        return res.status(200).json({ success: true, message: "Flashcard obtained", data: data });
     } catch (error) {
         console.log("Error in getting Flashcard: " + error.message);
-        return res.status(404).json({success:false, message:"Flashcard not found"});
+        return res.status(404).json({ success: false, message: "Flashcard not found" });
     }
 }
 
@@ -16,17 +16,44 @@ export async function getFlashcardsbyGroup(req, res) {
         const { id } = req.params; // id della collezione
         const flashcards = await Flashcard.find({ group: id });
 
-        if(flashcards === undefined || flashcards.length == 0) {
-            return res.status(400).json({success: false, message: "No flashcards found"});
+        if (flashcards === undefined || flashcards.length == 0) {
+            return res.status(400).json({ success: false, message: "No flashcards found" });
         }
 
-        return res.status(200).json({success: true, flashcards});
+        return res.status(200).json({ success: true, message: "Flashcards obtained", data: flashcards });
     } catch (error) {
         console.log("Error in getting flashcards: ", error.message);
-        return res.status(500).json({success:false, message: "Internal server error"});
+        return res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
 
-export async function addFlashcard(req,res){
+export async function addFlashcard(req, res) {
+    try {
+        const { group, title, definition } = req.body;
+
+        // Strings Checks
+        if (!group || !title || !definition) {
+            return res.status(400).json({ success: false, message: "All fields are required" });
+        }
+
+        const newFlashcard = new Flashcard({
+            group,
+            title,
+            definition
+        });
+        await newFlashcard.save();
+        return res.status(201).json({ success: true, message: "New flashcard created successfully" });
+
+    } catch (error) {
+        console.log("Error in creating new flashcard: ", error.message);
+        return res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
+
+export async function modifyFlashcard(req, res) {
+    return;
+}
+
+export async function deleteFlashcard(req, res) {
     return;
 }
