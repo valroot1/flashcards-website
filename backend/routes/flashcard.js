a4e2bdc223538bcea5b4171c46655dcbb10b3cf0
val <valeriabaleanu@gmail.com>
2025-10-03 16:33:09 +0200

    Update controllers/flashcard per utilizzare il metodo addFlashcard

--- backend/routes/flashcard.js
+++ backend/routes/flashcard.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getFlashcardbyId, getFlashcardsbyGroup } from '../controllers/flashcard.js';
+import { getFlashcardbyId, getFlashcardsbyGroup, addFlashcard } from '../controllers/flashcard.js';
 import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
 router.get("/getFlashcard/:id", protectRoute, getFlashcardbyId);
 router.get("/getFlashcards/:id", protectRoute, getFlashcardsbyGroup);
+router.post("/addFlashcard", protectRoute, addFlashcard);
 
 export default router;
