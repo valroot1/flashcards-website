44e4196f58b2d21c198f65258ae60d87c69e2ebc
val <valeriabaleanu@gmail.com>
2025-10-04 10:54:19 +0200

    Update addGroup in controllers/group con aggiunta try-catch

--- backend/controllers/group.js
+++ backend/controllers/group.js
@@ -11,41 +11,46 @@ export async function getGroup(req, res) {
         }
 
         // Check if Group is linked to the user
-        const group = await Group.findOne({ _id: id, user: userId});
-        if(!group) {
-            return res.status(404).json({ success: false, message: "Invalid group id"});
+        const group = await Group.findOne({ _id: id, user: userId });
+        if (!group) {
+            return res.status(404).json({ success: false, message: "Invalid group id" });
         }
 
-        return res.status(200).json({ success:true, message: "Group obtained", data: group});
+        return res.status(200).json({ success: true, message: "Group obtained", data: group });
 
     } catch (error) {
         console.log("Error in retrieving Group: ", error.message);
-        return res.status(500).json({ success: false, message: "Internal server error"});
+        return res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
 
 export async function addGroup(req, res) {
-    const { user, name, description } = req.body;
+    try {
+        const { user, name, description } = req.body;
 
-    // Input checks
-    if(!user || !name) {
-        return res.status(400).json({ success: false, message: "All fields are required"});
+        // Input checks
+        if (!user || !name) {
+            return res.status(400).json({ success: false, message: "All fields are required" });
+        }
+
+        // Check existing group
+        const existingGroup = await Group.findOne({ user: user, name: name });
+        if (existingGroup) {
+            return res.status(400).json({ success: false, message: "Name of group already exists" });
+        }
+
+        const newGroup = new Group({
+            user,
+            name,
+            description
+        });
+
+        await newGroup.save();
+        return res.status(201).json({ success: true, message: "New group created successfully" });
+    } catch (error) {
+        console.log("Error in creting Group: ", error.message);
+        return res.status(500).json({ success: false, message: "Internal server error" });
     }
-
-    // Check existing group
-    const existingGroup = await Group.findOne({ user: user, name: name });
-    if(existingGroup) {
-        return res.status(400).json({ success:false, message: "Name of group already exists"});
-    }
-
-    const newGroup = new Group({
-        user,
-        name,
-        description
-    });
-
-    await newGroup.save();
-    return res.status(201).json({ success: true, message: "New group created successfully"});
 }
 
 export async function modifyGroup(req, res) { }
