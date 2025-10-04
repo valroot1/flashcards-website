1b0e91fd44be6a773d766df5a78889b6ffe43204
val <valeriabaleanu@gmail.com>
2025-10-04 15:35:34 +0200

    Aggiunta getAllGroups in controllers/group

--- backend/controllers/group.js
+++ backend/controllers/group.js
@@ -1,6 +1,23 @@
 import mongoose from 'mongoose';
 import { Group } from "../models/group.js";
 
+export async function getAllGroups(req,res) {
+    try {
+        const userId = req.user._id;
+
+        const groups = await Group.find({ user: userId });
+
+        if(!group) {
+            return res.status(404).json({ success: false, message: "Group not found" });
+        }
+
+        return res.status(200).json({ success: true, message: "All group obtained", data: groups});
+    } catch (error) {
+        console.log("Error in retrieving Groups: ", error.message);
+        return res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
+
 export async function getGroup(req, res) {
 
     try {
