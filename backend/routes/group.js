01e0092d35e2d93721696ec737d78510480f94da
val <valeriabaleanu@gmail.com>
2025-10-04 11:46:03 +0200

    Update routes/group
    Aggiunta route per modifyGroup

--- backend/routes/group.js
+++ backend/routes/group.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
-import { addGroup, getGroup } from '../controllers/group.js';
+import { addGroup, getGroup, modifyGroup } from '../controllers/group.js';
 
 const router = express.Router();
 
 router.get("/getGroup/:id", protectRoute, getGroup);
 router.post("/addGroup", protectRoute, addGroup);
+router.post("/modifyGroup", protectRoute, modifyGroup);
 
 export default router;
