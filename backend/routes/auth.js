973e4da8fa127bea106ebd86402896dc70f4c58b
val <valeriabaleanu@gmail.com>
2025-10-11 18:16:23 +0200

    Update routes/auth con aggiunta route per authCheck

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { login, logout, signup } from '../controllers/auth.js';
+import { authCheck, login, logout, signup } from '../controllers/auth.js';
+import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.get("/authCheck", protectRoute, authCheck);
 
 export default router;
