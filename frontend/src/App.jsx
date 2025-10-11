aad51234f71729f6a5244ea38ae7485e89e1449b
val <valeriabaleanu@gmail.com>
2025-10-11 21:24:03 +0200

    Update App.jsx
    Aggiunta implemementazione authCheck

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,30 @@ import SignUpPage from "./pages/SignUpPage"
 import HomePage from "./pages/home/HomePage"
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authUser";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
 
 function App() {
+
+  const { user, isCheckingAuth, authCheck } = useAuthStore();
+
+  useEffect(() => {
+    console.log("useEffect authCheck chiamato");
+    authCheck();
+  }, [authCheck]);
+
+  if (isCheckingAuth) {
+    return (
+      <div className='h-screen'>
+        <div className='flex justify-center items-center bg-black h-full'>
+          <Loader className='animate-spin text-red-600 size-10' />
+        </div>
+      </div>
+    );
+  }
+
+  console.log("auth user is here: ", user);
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
       <div className="flex-1">
