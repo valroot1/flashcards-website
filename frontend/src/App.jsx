d5db40ac5356d7e553475715d8125895b877944f
val <valeriabaleanu@gmail.com>
2025-10-11 12:20:00 +0200

    Update App.jsx per contenere footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
 import { Route, Routes } from "react-router-dom"
 import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/home/HomePage"
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage/>} />
-      <Route path='/login' element={<LoginPage/>} />
-      <Route path='/signup' element={<SignUpPage/>} />
-    </Routes> 
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <div className="flex-1">
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
