fc0fbe05f87bf839d5e83a5ee13000a1d3309bed
val <valeriabaleanu@gmail.com>
2025-10-12 15:07:30 +0200

    Implementazione GroupsPreview in HomeScreen

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import AuthScreen from './AuthScreen';
+import { useAuthStore } from '../../store/authUser';
+import GroupsPreview from '../../components/GroupsPreview';
 
 // ONLY VISIBLE FOR AUTHENTICATED USER
 
 const HomeScreen = () => {
+  const { user } = useAuthStore();
   return (
     <div className="w-full font-[Nunito] flex flex-col">
-      CIAO
+      <div className="w-full max-w-md p-8">
+        <h2 className="text-2xl font-bold mb-6 text-left">
+          Hi, {user.username}!
+        </h2>
+        <h3>
+          Your study groups
+        </h3>
+        <hr className='border-t-2 md:w-auto'/>
+        <GroupsPreview />
+      </div>
     </div>
   );
 };
